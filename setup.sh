db0057b869bee6a829062972fb11f66b8ba6fcc9
Nghia Tran <nghia@yelp.com>
2018-01-08 16:10:49 -0800

    init

---
+++ setup.sh
@@ -0,0 +1,1 @@
+ln bash.txt ~/.bash_profile && ln alias.txt ~/.bash_alias && ln functions.txt ~/.bash_functions &&  cat .gitconfig > ~/.gitconfig
---
+++ unsinstall.sh
@@ -0,0 +1,1 @@
+rm ~/.bash_profile ~/.bash_alias ~/.bash_functions ~/.gitconfig
